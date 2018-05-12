4eaf708c739b7cd5490af923af0cab9c58f8f940
wangxu <wangxu@zdlhcar.com>
2018-05-12 15:23:24 +0800

    mac

---
+++ Chapter.5/5.5.4.a.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+name = ['anne', 'beth', 'george', 'damon']
+age = [12, 45, 32, 102]
+for i in range(len(name)):
+    print name[i], 'is', age[i], 'years old'
+
+print zip(name, age)
+for name, age in zip(name, age):
+    print name, 'is', age, 'years old'
